08d14205952128e46b7d3d0be384802ac4628aeb
Jaromir Obr <jaromir.obr@gmail.com>
2022-03-29 20:24:18 +0200

    Added highlighting

--- src/highlighting.rs
+++ src/highlighting.rs
@@ -1,16 +1,27 @@
 use termion::color;
 
-#[derive(PartialEq)]
+#[derive(PartialEq, Clone, Copy)]
 pub enum Type {
-    Number, // Highlighted number
-    Match,  // Highlighted match in search
-    None,   // No highlighting
+    Number,
+    String,
+    Character,
+    Comment,
+    MultilineComment,
+    PrimaryKeywords,
+    SecondaryKeywords,
+    Match, // Highlight match in search
+    None,  // No highlighting
 }
 
 impl Type {
-    pub fn to_color(&self) -> color::Rgb {
-        match *self {
+    pub fn to_color(self) -> color::Rgb {
+        match self {
             Type::Number => color::Rgb(220, 163, 163),
+            Type::String => color::Rgb(211, 54, 130),
+            Type::Character => color::Rgb(108, 113, 196),
+            Type::Comment | Type::MultilineComment => color::Rgb(0x67, 0x95, 0x4f),
+            Type::PrimaryKeywords => color::Rgb(181, 137, 0),
+            Type::SecondaryKeywords => color::Rgb(42, 161, 152),
             Type::Match => color::Rgb(38, 139, 210),
             Type::None => color::Rgb(255, 255, 255),
         }
--- src/row.rs
+++ src/row.rs
@@ -1,4 +1,5 @@
 use crate::highlighting;
+use crate::HighlightingOptions;
 use crate::SearchDirection;
 
 use std::cmp;
@@ -9,6 +10,7 @@ use unicode_segmentation::UnicodeSegmentation;
 pub struct Row {
     string: String,
     highlighting: Vec<highlighting::Type>,
+    pub is_highlighted: bool,
     len: usize,
 }
 
@@ -17,6 +19,7 @@ impl From<&str> for Row {
         Self {
             string: String::from(slice),
             highlighting: Vec::new(),
+            is_highlighted: false,
             len: slice.graphemes(true).count(),
         }
     }
@@ -115,7 +118,7 @@ impl Row {
     }
 
     #[allow(clippy::integer_arithmetic)]
-    pub fn split(&mut self, at: usize) -> Self {
+    pub(crate) fn split(&mut self, at: usize) -> Self {
         let mut row: String = String::new();
         let mut length = 0;
         let mut splitted_row: String = String::new();
@@ -134,6 +137,7 @@ impl Row {
         Self {
             string: splitted_row,
             highlighting: Vec::new(),
+            is_highlighted: false,
             len: splitted_length,
         }
     }
@@ -180,74 +184,359 @@ impl Row {
         None
     }
 
-    pub fn highlight(&mut self, word: Option<&str>) {
-        let mut highlighting = Vec::new();
-        let mut matches = Vec::new();
-        let mut search_index = 0;
+    #[allow(clippy::integer_arithmetic)]
+    pub fn highlight(
+        &mut self,
+        opts: &HighlightingOptions,
+        word: &Option<String>,
+        mut start_with_comment: bool,
+    ) -> bool {
+        let row = self.string.clone();
+        let graphemes = row.graphemes(true).collect::<Vec<&str>>();
 
-        if let Some(word_unwrapped) = word {
-            while let Some(search_match) =
-                self.find(word_unwrapped, search_index, SearchDirection::Forward)
+        if self.is_highlighted && word.is_none() {
+            if let Some(hl_type) = self.highlighting.last() {
+                return *hl_type == highlighting::Type::MultilineComment
+                    && self.len() > 1
+                    && if let Some(grapheme_asterisk) = graphemes.get(self.len() - 2) {
+                        if grapheme_asterisk.contains('*') {
+                            if let Some(grapheme_slash) = graphemes.get(self.len() - 1) {
+                                !grapheme_slash.contains('/')
+                            } else {
+                                true
+                            }
+                        } else {
+                            true
+                        }
+                    } else {
+                        true
+                    };
+            }
+        }
+
+        self.highlighting = Vec::new();
+        let mut index = 0;
+
+        #[allow(clippy::shadow_unrelated)]
+        while let Some(grapheme) = graphemes.get(index) {
+            let is_multiline_comment_present;
+            (is_multiline_comment_present, start_with_comment) = self.highlight_multiline_comment(
+                &mut index,
+                opts,
+                start_with_comment,
+                grapheme,
+                &graphemes,
+            );
+            if is_multiline_comment_present {
+                continue;
+            }
+
+            if self.highlight_char(&mut index, opts, grapheme, &graphemes)
+                || self.highlight_comment(&mut index, opts, grapheme, &graphemes)
+                || self.highlight_primary_keywords(&mut index, opts, &graphemes)
+                || self.highlight_secondary_keywords(&mut index, opts, &graphemes)
+                || self.highlight_string(&mut index, opts, grapheme, &graphemes)
+                || self.highlight_number(&mut index, opts, grapheme, &graphemes)
             {
-                matches.push(search_match);
+                continue;
+            }
+            self.highlighting.push(highlighting::Type::None);
+            index = index.saturating_add(1);
+        }
+
+        self.highlight_match(word);
+        self.is_highlighted = true;
+        start_with_comment
+    }
+
+    /// Find and highlight all search matches on current line
+    fn highlight_match(&mut self, word: &Option<String>) {
+        if let Some(ref word_unwrapped) = *word {
+            if word_unwrapped.is_empty() {
+                return;
+            }
+            let mut index = 0;
+            while let Some(search_match) =
+                self.find(word_unwrapped, index, SearchDirection::Forward)
+            {
                 if let Some(next_index) =
                     search_match.checked_add(word_unwrapped.graphemes(true).count())
                 {
-                    search_index = next_index;
+                    #[allow(clippy::indexing_slicing)]
+                    for i in search_match..next_index {
+                        self.highlighting[i] = highlighting::Type::Match;
+                    }
+                    index = next_index;
                 } else {
                     break;
                 }
             }
         }
+    }
 
-        let mut prev_is_separator = true;
-        let mut index = 0;
-        'outer: while let Some(grapheme) = self.string.graphemes(true).nth(index) {
-            if let Some(word_unwrapped) = word {
-                if matches.contains(&index) {
-                    for _ in word_unwrapped.graphemes(true) {
-                        highlighting.push(highlighting::Type::Match);
-                        if let Some(next_index) = index.checked_add(1) {
-                            index = next_index;
-                        } else {
-                            break 'outer;
-                        }
-                    }
-                    continue;
+    fn highlight_str(
+        &mut self,
+        index: &mut usize,
+        substring: &str,
+        graphemes: &[&str],
+        hl_type: highlighting::Type,
+    ) -> bool {
+        if substring.is_empty() {
+            return false;
+        }
+        for (substring_index, grapheme) in substring.graphemes(true).enumerate() {
+            if let Some(next_char) = graphemes.get(index.saturating_add(substring_index)) {
+                if *next_char != grapheme {
+                    return false;
                 }
-            }
-
-            let previous_highlight = if index > 0 {
-                #[allow(clippy::integer_arithmetic)]
-                highlighting
-                    .get(index - 1)
-                    .unwrap_or(&highlighting::Type::None)
             } else {
-                &highlighting::Type::None
-            };
-
-            if grapheme.chars().any(|c| c.is_ascii_digit())
-                && (prev_is_separator || previous_highlight == &highlighting::Type::Number)
-                || grapheme.chars().any(|c| c == '.')
-                    && previous_highlight == &highlighting::Type::Number
-            {
-                highlighting.push(highlighting::Type::Number);
-            } else {
-                highlighting.push(highlighting::Type::None);
-            }
-            prev_is_separator = grapheme
-                .chars()
-                .any(|c| c.is_ascii_punctuation() || c.is_ascii_whitespace());
-            if let Some(next_index) = index.checked_add(1) {
-                index = next_index;
-            } else {
-                break;
+                return false;
             }
         }
-        self.highlighting = highlighting;
+        for _ in 0..substring.len() {
+            self.highlighting.push(hl_type);
+            *index = index.saturating_add(1);
+        }
+        true
+    }
+
+    fn highlight_keywords(
+        &mut self,
+        index: &mut usize,
+        graphemes: &[&str],
+        keywords: &[(String, usize)],
+        hl_type: highlighting::Type,
+    ) -> bool {
+        if *index > 0 {
+            if let Some(prev_grapheme) = graphemes.get(index.saturating_sub(1)) {
+                if !is_separator(prev_grapheme) {
+                    return false;
+                }
+            }
+        }
+        for word in keywords {
+            // Originally (when keywords was an array of strings only) I used `let word_len = word.graphemes(true).count()`, but it causes very slow search. Moreover it's useless as keywords are ascii only
+            let word_len = word.1;
+            if *index < self.len().saturating_sub(word_len) {
+                if let Some(next_grapheme) = graphemes.get(index.saturating_add(word_len)) {
+                    if !is_separator(next_grapheme) {
+                        continue;
+                    }
+                }
+            }
+            if self.highlight_str(index, &word.0, graphemes, hl_type) {
+                return true;
+            }
+        }
+        false
+    }
+
+    fn highlight_primary_keywords(
+        &mut self,
+        index: &mut usize,
+        opts: &HighlightingOptions,
+        graphemes: &[&str],
+    ) -> bool {
+        self.highlight_keywords(
+            index,
+            graphemes,
+            opts.primary_keywords(),
+            highlighting::Type::PrimaryKeywords,
+        )
+    }
+    fn highlight_secondary_keywords(
+        &mut self,
+        index: &mut usize,
+        opts: &HighlightingOptions,
+        graphemes: &[&str],
+    ) -> bool {
+        self.highlight_keywords(
+            index,
+            graphemes,
+            opts.secondary_keywords(),
+            highlighting::Type::SecondaryKeywords,
+        )
+    }
+
+    fn highlight_char(
+        &mut self,
+        index: &mut usize,
+        opts: &HighlightingOptions,
+        grapheme: &str,
+        graphemes: &[&str],
+    ) -> bool {
+        if opts.characters() && grapheme.chars().any(|c| c == '\'') {
+            if let Some(next_grapheme) = graphemes.get(index.saturating_add(1)) {
+                let closing_index = if next_grapheme.chars().any(|c| c == '\\') {
+                    index.saturating_add(3)
+                } else {
+                    index.saturating_add(2)
+                };
+                if let Some(closing_grapheme) = graphemes.get(closing_index) {
+                    if closing_grapheme.chars().any(|c| c == '\'') {
+                        for _ in 0..=closing_index.saturating_sub(*index) {
+                            self.highlighting.push(highlighting::Type::Character);
+                            *index = index.saturating_add(1);
+                        }
+                        return true;
+                    }
+                }
+            };
+        }
+        false
+    }
+
+    fn highlight_comment(
+        &mut self,
+        index: &mut usize,
+        opts: &HighlightingOptions,
+        grapheme: &str,
+        graphemes: &[&str],
+    ) -> bool {
+        if opts.comments() && grapheme.chars().any(|c| c == '/') {
+            if let Some(next_grapheme) = graphemes.get(index.saturating_add(1)) {
+                if next_grapheme.chars().any(|c| c == '/') {
+                    for _ in *index..self.len() {
+                        self.highlighting.push(highlighting::Type::Comment);
+                        *index = index.saturating_add(1);
+                    }
+                    return true;
+                }
+            };
+        }
+        false
+    }
+
+    fn highlight_multiline_comment(
+        &mut self,
+        index: &mut usize,
+        opts: &HighlightingOptions,
+        mut start_with_comment: bool,
+        grapheme: &str,
+        graphemes: &[&str],
+    ) -> (bool, bool) {
+        if start_with_comment {
+            *index = self.len();
+            for (index_iter, grapheme_iter) in graphemes.iter().enumerate() {
+                if grapheme_iter.chars().any(|c| c == '*') {
+                    if let Some(closing_slash) = graphemes.get(index_iter.saturating_add(1)) {
+                        if closing_slash.chars().any(|c| c == '/') {
+                            start_with_comment = false;
+                            *index = index_iter.saturating_add(2);
+                            break;
+                        }
+                    }
+                }
+            }
+            for _ in 0..*index {
+                self.highlighting.push(highlighting::Type::MultilineComment);
+            }
+            return (true, start_with_comment);
+        }
+
+        start_with_comment = true;
+        if opts.multiline_comments() && grapheme.chars().any(|c| c == '/') {
+            if let Some(next_grapheme) = graphemes.get(index.saturating_add(1)) {
+                if next_grapheme.chars().any(|c| c == '*') {
+                    let mut closing_index = self.len();
+                    for (index_iter, grapheme_iter) in
+                        graphemes.iter().skip(index.saturating_add(2)).enumerate()
+                    {
+                        if grapheme_iter.chars().any(|c| c == '*') {
+                            if let Some(closing_slash) =
+                                graphemes.get(index.saturating_add(index_iter.saturating_add(3)))
+                            {
+                                if closing_slash.chars().any(|c| c == '/') {
+                                    closing_index =
+                                        index.saturating_add(index_iter.saturating_add(4));
+                                    start_with_comment = false;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                    for _ in *index..closing_index {
+                        self.highlighting.push(highlighting::Type::MultilineComment);
+                    }
+                    *index = closing_index;
+                    return (true, start_with_comment);
+                }
+            }
+        };
+        (false, false)
+    }
+
+    fn highlight_string(
+        &mut self,
+        index: &mut usize,
+        opts: &HighlightingOptions,
+        grapheme: &str,
+        graphemes: &[&str],
+    ) -> bool {
+        if opts.strings() & grapheme.chars().any(|c| c == '"') {
+            loop {
+                self.highlighting.push(highlighting::Type::String);
+                *index = index.saturating_add(1);
+                if let Some(next_grapheme) = graphemes.get(*index) {
+                    // '\a' or '\"' is a part of string
+                    if next_grapheme.chars().any(|c| c == '\\') {
+                        self.highlighting.push(highlighting::Type::String);
+                        *index = index.saturating_add(1);
+                    } else if next_grapheme.chars().any(|c| c == '"') {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+            self.highlighting.push(highlighting::Type::String);
+            *index = index.saturating_add(1);
+            return true;
+        }
+        false
+    }
+
+    fn highlight_number(
+        &mut self,
+        index: &mut usize,
+        opts: &HighlightingOptions,
+        grapheme: &str,
+        graphemes: &[&str],
+    ) -> bool {
+        if opts.numbers() && grapheme.chars().any(|c| c.is_ascii_digit()) {
+            if *index > 0 {
+                if let Some(prev_grapheme) = graphemes.get(index.saturating_sub(1)) {
+                    if !is_separator(prev_grapheme) {
+                        return false;
+                    }
+                }
+            }
+            loop {
+                self.highlighting.push(highlighting::Type::Number);
+                *index = index.saturating_add(1);
+                if let Some(next_grapheme) = graphemes.get(*index) {
+                    if next_grapheme
+                        .chars()
+                        .any(|c| c != '.' && !c.is_ascii_digit())
+                    {
+                        break;
+                    }
+                } else {
+                    break;
+                }
+            }
+            return true;
+        }
+        false
     }
 
     pub fn get_string(&self) -> String {
         self.string.clone()
     }
+}
+
+fn is_separator(grapheme: &str) -> bool {
+    grapheme
+        .chars()
+        .any(|c| c.is_ascii_punctuation() || c.is_ascii_whitespace())
 }
